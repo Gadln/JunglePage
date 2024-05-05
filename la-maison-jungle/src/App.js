2914a01e678aafd92f5fcb3cecfaf9ff43a65357
Gadln <helene.gadessaud@icloud.com>
2024-05-05 22:13:35 +0200

    add components React for the app

---
+++ la-maison-jungle/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Banner from './components/Banner';
+import Cart from './components/cart';
+
+function App() {
+  return (<React.Fragment>
+   <Banner />
+   <Cart />
+   </React.Fragment>
+  );
+}
+
+export default App;
---
+++ la-maison-jungle/src/components/cart.js
@@ -0,0 +1,17 @@
+function Cart() {
+    const monsteraPrice = 8
+    const ivyPrice = 10
+    const flowerPrice = 15
+    return (<div>
+      <h2>Panier</h2>
+        <ul>
+          <li>Monstera :{ monsteraPrice } euros</li>
+          <li>Lierre :{ ivyPrice } euros</li>
+          <li>Bouquet de fleurs :{ flowerPrice } euros</li>
+      </ul>
+      Total: {monsteraPrice + ivyPrice + flowerPrice } euros
+      </div>
+      )
+}
+
+export default Cart
