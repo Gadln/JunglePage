9d7bf9e7239b9ba2f9a4c171c522e60e8e4018ce
Gadln <helene.gadessaud@icloud.com>
2024-05-06 15:26:38 +0200

    Add option solde

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -17,7 +17,10 @@ function ShoppingList() {
 			</ul>
 			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>{plant.name}</li>
+					<li key={plant.id} className='lmj-plant-item'>{plant.name}
+          {plant.name}
+          {plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>} 
+          </li>
 				))}
 			</ul>
 		</div>
