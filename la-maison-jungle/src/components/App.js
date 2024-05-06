6c48e3fe2d1cfd1dc587be1d9a29259add545325
Gadln <helene.gadessaud@icloud.com>
2024-05-06 09:30:46 +0200

    Add components ShoppingList

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/App.css';
 import Banner from './Banner';
 import Cart from './cart';
+import ShoppingList from './ShoppingList';
 
 function App() {
   return (<React.Fragment>
